0d11627901a0cb0e43c12524cb957b576115a412
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2021-10-11 02:12:03 +0300

    Create FieldModifyItem.php

---
+++ mir/common/FieldModifyItem.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tatiana
+ * Date: 04.12.17
+ * Time: 15:28
+ */
+
+namespace mir\common;
+
+/*
+ * Object of changing value in field
+ * */
+class FieldModifyItem
+{
+    protected $sign;
+    protected $val;
+    protected $percent;
+
+    public function __construct($sign, $val, $percent = false)
+    {
+        $this->val = $val;
+        $this->sign = $sign;
+        $this->percent = $percent;
+    }
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+}

5a923de427e79fc6d489da7556d82ce982da94e7
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2021-10-11 02:03:27 +0300

    Create index.php

---
+++ http/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use GuzzleHttp\Psr7\ServerRequest;
+use mir\config\Conf;
+
+$GLOBALS['mktimeStart'] = microtime(true);
+
+ignore_user_abort(false);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (!class_exists(Conf::class)) {
+    $Config = null;
+    list($module, $lastPath) = ['install', ''];
+} else {
+    $Config = new Conf();
+    if (is_callable([$Config, 'setHostSchema'])) {
+        $Config->setHostSchema($_SERVER['HTTP_HOST']);
+    }
+    list($module, $lastPath) = $Config->getActivationData($_SERVER['REQUEST_URI']);
+}
+
+
+if (empty($module)) {
+    $module = 'Table';
+    $lastPath = '';
+}
+$controllerClass = 'mir\\moduls\\' . $module . '\\' . $module . 'Controller';
+if (class_exists($controllerClass)) {
+    /*
+     * @var Controller $Controller
+     * */
+    $Controller = new $controllerClass($Config);
+
+    $request = ServerRequest::fromGlobals();
+    $response = $Controller->doIt($request, true);
+
+
+    //$Config->getSql()->transactionRollBack();
+
+
+    die;
+} else die('Не найдено ' . htmlspecialchars($controllerClass));
+
+?>

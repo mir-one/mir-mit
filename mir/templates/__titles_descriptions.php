b6d94e7adae480f4053716862d33c18935115634
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2021-10-11 11:16:44 +0300

    Update __titles_descriptions.php

--- mir/templates/__titles_descriptions.php
+++ mir/templates/__titles_descriptions.php
@@ -2,7 +2,7 @@
 $host = 'http' . (!empty($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . '/';
 $settings['h_og_title'] = $settings['h_og_title'] ?? 'Mir — конструктор CRM, ERP, XRM.';
 if (empty($settings['h_title'])) {
-    $settings['h_title'] = 'Mir';
+    $settings['h_title'] = 'MIR';
 }
 $settings['h_og_description'] = $settings['h_og_description'] ?? 'Разрабатывайте управленческие инструменты х5 раз быстрее.';
 $settings['h_og_image'] = empty($settings['h_og_image'][0]['file']) ? 'imgs/mir.png' : 'fls/' . $settings['h_og_image'][0]['file'];
